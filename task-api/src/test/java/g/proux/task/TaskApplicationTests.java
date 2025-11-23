f2586d755ef69f69781b73b328a80309566f28ef
Guigz <g.proux.delrouyre@gmail.com>
2025-11-23 15:59:01 +0100

    build: init project with H2, JPA and Lombok dependencies

---
+++ task-api/src/test/java/g/proux/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package g.proux.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
